3fc8c5812a5ee899c35d99265a65b161f6ef89d2
StavrosArm <75743598+StavrosArm@users.noreply.github.com>
2025-03-29 15:28:51 +0200

    Fixed the configuration read

--- model/model.py
+++ model/model.py
@@ -5,10 +5,10 @@ from transformers import AutoModel
 class MusicModel(nn.Module):
     def __init__(self, config):
         super().__init__()
-        model = AutoModel.from_pretrained(config.model_name)
+        self.model = AutoModel.from_pretrained(config.model.model_name)
 
     def forward(self, x: torch.tensor) -> torch.Tensor:
-        return x
+        return self.model(*x)
 
     def __call__(self, input: torch.Tensor) -> torch.Tensor:
         return self.forward(input)
