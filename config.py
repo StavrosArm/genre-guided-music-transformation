da9418efc99add2fd05cf94c617137022260d16f
StavrosArm <stavros.armeniakos@orfium.com>
2025-05-13 15:38:40 +0300

    Fixed the configuration class

--- config.py
+++ config.py
@@ -18,27 +18,19 @@ class Namespace:
 class Config:
     def __init__(self, config_path):
         self.config_path = config_path
-        self.model = None
-        self.optimizer = None
-        self.loss = None
-        self.dataset = None
-        self.feature_extractor = None
-        self.training = None
-        self.experiment = None
-
         self.load_config()
 
     def load_config(self):
         with open(self.config_path, 'r') as f:
             cfg = yaml.safe_load(f)
 
-        self.model = Namespace(cfg.get("model", {}))
-        self.optimizer = Namespace(cfg.get("optimizer", {}))
-        self.loss = cfg.get("loss", "")
-        self.dataset = Namespace(cfg.get("dataset", {}))
-        self.feature_extractor = Namespace(cfg.get("feature_extractor", {}))
-        self.training = Namespace(cfg.get("training", {}))
-        self.experiment = Namespace(cfg.get("experiment", {}))
+        for section in ["model", "optimizer", "loss", "dataset", "feature_extractor", "training", "experiment"]:
+            setattr(self, section, Namespace(cfg.get(section, {})))
 
     def __repr__(self):
         return f"<Config from {self.config_path}>"
+
+
+if __name__ == "__main__":
+    config = Config("config.yaml")
+    print(config)
