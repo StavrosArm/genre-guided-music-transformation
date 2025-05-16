f8b1f111785924977a919b731c505679f0295ea5
StavrosArm <stavros.armeniakos@orfium.com>
2025-05-16 12:59:49 +0300

    Fixed the padding to 10 seconds of audio

--- utils/audio_utils.py
+++ utils/audio_utils.py
@@ -1,8 +1,7 @@
 import torch
 import torchaudio
 
-
-def prepare_audio(audio, sample_rate, start, end, noise_ratio, target_sr=16000):
+def prepare_audio(audio, sample_rate, start, end, noise_ratio, target_sr=16000, fixed_length=160000):
     if audio.ndim > 1 and audio.shape[0] > 1:
         audio = torch.mean(audio, dim=0, keepdim=True)
 
@@ -12,8 +11,15 @@ def prepare_audio(audio, sample_rate, start, end, noise_ratio, target_sr=16000):
 
     audio = audio[:, start:end]
 
+    if audio.shape[1] < fixed_length:
+        pad_size = fixed_length - audio.shape[1]
+        audio = torch.nn.functional.pad(audio, (0, pad_size))
+    elif audio.shape[1] > fixed_length:
+        audio = audio[:, :fixed_length]
+
     if noise_ratio > 0:
         noise = torch.randn_like(audio) * noise_ratio
         audio = audio + noise
 
-    return audio
+    return audio.squeeze(0)
+
