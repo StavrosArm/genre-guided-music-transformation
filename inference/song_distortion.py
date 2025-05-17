3430b80ba61615f293dfce762cdd762bfcb0d6a3
StavrosArm <stavros.armeniakos@orfium.com>
2025-05-17 12:48:27 +0300

    Created inference scripts.

--- inference/song_distortion.py
+++ inference/song_distortion.py
@@ -1,4 +1,7 @@
+from model.model import MusicModel
+from utils.audio_utils import prepare_audio
 
 
 def distort_songs(config):
+    #TODO: Implement logic of the input waveform distortion
     pass
